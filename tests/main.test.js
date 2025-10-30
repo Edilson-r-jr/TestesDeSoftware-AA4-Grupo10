441955ae756f05569f1f9e72626ed3cefed71820
LucasAlves71 <lucasalves2005@gmail.com>
2025-10-30 15:20:23 -0300

    Ciclo 1

--- tests/main.test.js
+++ tests/main.test.js
@@ -1,5 +1,10 @@
-const sum = require("../src/main").sum;
+const main = require("../src/main");
 
 test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+  expect(main.sum(1, 2)).toBe(3);
+});
+
+// TDD: first failing test for expression validation
+test('valid simple expression "(1+2)+3" should return true', () => {
+  expect(main.isValidExpression("(1+2)+3")).toBe(true);
 });
